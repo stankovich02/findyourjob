3841bd19f946420f8a67505062eef2d78028389c
Marko Stankovic <marefaca2002@gmail.com>
2024-02-28 22:38:02 +0100

    Adding favicon to project

--- resources/views/fixed/client/head.blade.php
+++ resources/views/fixed/client/head.blade.php
@@ -8,7 +8,7 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <!-- Favicon -->
-<link href="assets/img/favicon.ico" rel="icon">
+<link href="{{asset("assets/img/favicon.ico")}}" rel="icon">
 
 <!-- Google Web Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
